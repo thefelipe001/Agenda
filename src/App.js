77db1fbc473dd531892c7e0131c127bf27723095
thefelipe001 <61038817+thefelipe001@users.noreply.github.com>
2020-03-15 16:06:18 -0400

    Agenda
    Agenda react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contacto from './Componentes/Contacto';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Contacto/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
